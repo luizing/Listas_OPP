b153c0e0334126e4a415532b9dc31fd2f3123f9a
Luiz Eduardo <83519960+luizing@users.noreply.github.com>
2024-10-21 10:44:15 -0300

    Update main.java

--- Lista3/src/Q4_/main.java
+++ Lista3/src/Q4_/main.java
@@ -2,22 +2,30 @@
 para calcular a área do círculo e retornar esse valor, utilizando a seguinte fórmula área π raio²(questao2). Escreva 
 um programa Java que crie 5 objetos Círculo de tamanhos diferentes, insira os em uma lista e depois 
 percorra a lista imprimindo a área de cada círculo armazenado. */
-package Q4_;
+package Q4_ColecaoCirculos;
 import java.util.*;
 
 
 
 import Q2_Areas.Circulo;
 
-public class main {
-    Circulo A = new Circulo(3);
-    Circulo B = new Circulo(5);
-    Circulo C = new Circulo(4);
-    Circulo D = new Circulo(10);
-    Circulo E = new Circulo(1);
+public class Q4_main {
+    public static void main(String[] args) {
+        Circulo A = new Circulo(3);
+        Circulo B = new Circulo(5);
+        Circulo C = new Circulo(4);
+        Circulo D = new Circulo(10);
+        Circulo E = new Circulo(1);
 
-    ArrayList<Circulo> lista = new ArrayList<Circulo>();
+        ArrayList<Circulo> lista = new ArrayList<Circulo>();
 
-    lista.add(A);
+        lista.add(A);
+        lista.add(B);
+        lista.add(C);
+        lista.add(D);
+        lista.add(E);
 
+        lista.forEach(i -> {System.out.println(i.calculaArea());});
+        
+}
 }
